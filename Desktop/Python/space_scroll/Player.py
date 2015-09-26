437faefd699172af92b36f84dd30893ac927fe50
YJoe <YJoe762@gmail.com>
2015-09-26 10:57:09 +0100

    Update Player.py
    Health bar, bullet collide

--- Desktop/Python/space_scroll/Player.py
+++ Desktop/Python/space_scroll/Player.py
@@ -2,6 +2,25 @@ from Start_up import *
 from Bullet import Bullet
 from Upgrades import*
 
+class HealthBar:
+    def __init__(self, player):
+        self.x = 100
+        self.y = 10
+        self.player = player
+        self.bar = pygame.Surface((20 * self.player.health, 10))
+        self.bar.fill((0, 255, 0))
+
+        self.bar_back = pygame.Surface((21 * self.player.health, 20))
+        self.bar_back.fill((50, 50, 50))
+
+    def update_health(self):
+        self.bar = pygame.Surface((20 * self.player.health, 10))
+        self.bar.fill((0, 255, 0))
+
+    def display(self):
+        main_s.blit(self.bar_back, (self.x - 5, self.y - 5))
+        main_s.blit(self.bar, (self.x, self.y))
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -15,6 +34,33 @@ class Player(pygame.sprite.Sprite):
         self.cool_counter = 0
         self.bullets_used = 0
         self.money = 0
+        self.health = 10
+        self.alive = True
+        self.hit = False
+
+        self.s_upgrade = 0
+        self.m_upgrade = 0
+        self.g_s_upgrade = 0
+        self.g_p_upgrade = 0
+
+        self.speed = upgrades_speed[self.s_upgrade]
+        self.money_collection = upgrades_money_collection[self.m_upgrade]
+        self.cool_time = upgrades_gun_speed[self.g_s_upgrade]
+        self.bullet_power = upgrades_gun_power[self.g_p_upgrade]
+
+    def reset(self):
+        self.surface = pygame.Surface((20, 20))
+        self.surface.fill(main_theme)
+        self.rect = self.surface.get_rect()
+        self.rect.x = 10
+        self.rect.y = height/2
+        self.dy = 0
+        self.cool = True
+        self.cool_counter = 0
+        self.bullets_used = 0
+        self.money = 0
+        self.health = 10
+        self.alive = True
 
         self.s_upgrade = 0
         self.m_upgrade = 0
@@ -37,11 +83,31 @@ class Player(pygame.sprite.Sprite):
         # multiply all coins by the money collection upgrade
         self.money += coins * self.money_collection
 
+    def check_collide(self, bullet_list):
+        self.hit = False
+        # check the enemy against all off the bullets in the bullet list
+        for x in range(0, len(bullet_list)):
+            # check that the bullet was shot from a
+            # player and should damage the enemy
+            if bullet_list[len(bullet_list) - x - 1].shot_from == "Enemy":
+                if pygame.sprite.collide_rect(self, bullet_list[len(bullet_list) - x - 1]):
+                    # if a bullet has collided remove the correct amount of health based
+                    # off of the power of the players bullets
+                    self.health -= 1
+                    self.hit = True
+                    print(self.health)
+                    if self.health == 0:
+                        self.alive = False
+                    # if the enemy will still be alive set its new colour based off of
+                    # its new health value
+                    # remove the bullet
+                    del bullet_list[len(bullet_list) - x - 1]
+
     def shoot(self, bullet_list):
         # check the gun can shoot
         if self.cool:
             # create a new bullet and add it to the games bullet list
-            new_bullet = Bullet(self.rect.center, self.bullet_power)
+            new_bullet = Bullet(self.rect.center, self.bullet_power, "Player")
             bullet_list.append(new_bullet)
             # set the gun to not able to shoot
             self.cool = False
@@ -108,10 +174,11 @@ class Player(pygame.sprite.Sprite):
                 self.collect_package(package_list[len(package_list) - i - 1].holds, note_controller)
                 del package_list[len(package_list) - i - 1]
 
-    def update(self, package_list, note_controller):
+    def update(self, package_list, note_controller, bullet_list):
         # update the player object
         self.check_package_collide(package_list, note_controller)
         self.check_cool_down()
+        self.check_collide(bullet_list)
         if self.money < 0:
             self.money = 0
         self.rect.y += self.dy
