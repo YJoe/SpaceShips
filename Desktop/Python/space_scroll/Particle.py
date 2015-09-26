80bc20f086f3f4b464d590692c9b0079b45f576a
YJoe <YJoe762@gmail.com>
2015-09-26 10:55:39 +0100

    Update Particle.py
    Player can now explode, particles will be the player's colour

--- Desktop/Python/space_scroll/Particle.py
+++ Desktop/Python/space_scroll/Particle.py
@@ -2,11 +2,16 @@ from Start_up import*
 
 
 class Particle(pygame.sprite.Sprite):
-    def __init__(self, size, velocity, origin):
+    def __init__(self, size, velocity, origin, colour):
         pygame.sprite.Sprite.__init__(self)
         self.surface = pygame.Surface((size, size))
-        bright = random.randint(0, 205)
-        self.surface.fill((50 + bright, 50 + bright, 50 + bright))
+        if colour == (0, 100, 100):
+            bright = random.randint(0, 50)
+            self.surface.fill((50, 50 + bright, 50 + bright))
+        else:
+            bright = random.randint(0, 205)
+            self.surface.fill((50 + bright, 50 + bright, 50 + bright))
+
         self.rect = self.surface.get_rect()
         self.rect.x, self.rect.y = origin.center
         self.dx = velocity[0]
