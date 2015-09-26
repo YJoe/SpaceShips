ef9279b7cbb366ac7813040c530a19ec3a7789c2
YJoe <YJoe762@gmail.com>
2015-09-26 10:51:13 +0100

    Update Loop.py

--- Desktop/Python/space_scroll/Loop.py
+++ Desktop/Python/space_scroll/Loop.py
@@ -1,14 +1,7 @@
 from Start_up import*
 from Player import Player
-from Menu import InGameMain, MainMenu, Shop, InfoMenu
+from Menu import InGameMain, MainMenu, Shop, InfoMenu, SettingsMenu, GameOver
 from Game import Game
-
-
-def reset_next_states(to_reset):
-    # reset all state objects so that "next_state"
-    # is reverted to the object's own state
-    for i in range(0, len(to_reset)):
-        to_reset[i].next_state = to_reset[i].own_state
 
 # create all state objects
 player = Player()
@@ -17,6 +10,14 @@ info = InfoMenu()
 game = Game(player)
 paused = InGameMain()
 shop = Shop(player)
+settings = SettingsMenu()
+game_over = GameOver()
+
+def reset_next_states(to_reset):
+    # reset all state objects so that "next_state"
+    # is reverted to the object's own state
+    for i in range(0, len(to_reset)):
+        to_reset[i].next_state = to_reset[i].own_state
 
 # set state to the home (main menu) state
 state = home_state
@@ -29,13 +30,16 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
-                running = False
+                game.player.alive = False
 
     # control the game state
     # an object will always return its own state
     # number unless input events set otherwise
     if state == home_state:
         state = home.run(event_list)
+        player.reset()
+        game.reset(player)
+        shop.reset(player)
     elif state == info_state:
         state = info.run(event_list)
     elif state == game_state:
@@ -44,11 +48,20 @@ while running:
         state = paused.run(event_list)
     elif state == shop_state:
         state = shop.run(event_list)
+    elif state == settings_state:
+        state = settings.run(event_list)
+    elif state == game_over_state:
+        state = game_over.run(event_list)
+    elif state == reset_game_state:
+        player.reset()
+        game.reset(player)
+        shop.reset(player)
+        state = game_state
     elif state == quit_state:
         running = False
 
     # ensure all "next_state" variables are set to the objects own state
-    reset_next_states([home, info, game, paused, shop])
+    reset_next_states([home, info, game, paused, shop, settings, game_over])
     pygame.display.flip()
 
 # exit all PyGame modules
