a44e438bc84b46f83a80e251e74f3727db2563cf
YJoe <YJoe762@gmail.com>
2015-09-26 10:48:32 +0100

    Originate from enemy or player
    Enemies can now create instances of bullets.

--- Desktop/Python/space_scroll/Bullet.py
+++ Desktop/Python/space_scroll/Bullet.py
@@ -2,13 +2,23 @@ from Start_up import*
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, pos, power):
+    def __init__(self, pos, power, shot_from):
         self.surface = pygame.Surface((5, 2))
         self.surface.fill((200, 200, 200))
         self.rect = self.surface.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
-        self.dx = 5
+
+        # the direction could just be controlled here but then
+        # all bullets would damage anything, which I know is
+        # more realistic but it would become too easy as enemies
+        # could kill each other. maybe I'll add that as a setting
+        # for the user to adjust if they want too.
+        self.shot_from = shot_from
+        if self.shot_from == "Player":
+            self.dx = 5
+        elif self.shot_from == "Enemy":
+            self.dx = -5
 
         self.power = power
 
