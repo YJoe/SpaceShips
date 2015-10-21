d01f404941176ee6ea2637508fbaa8b3885be324
Joe <YJoe762@gmail.com>
2015-10-21 11:08:24 +0100

    Health packs included

--- Desktop/Python/space_scroll/Package.py
+++ Desktop/Python/space_scroll/Package.py
@@ -5,11 +5,9 @@ from Upgrades import*
 class Package(pygame.sprite.Sprite):
     def __init__(self, dx, pos):
         self.image = pygame.Surface((10, 10))
-        self.image.fill((0, 70, 0))
         self.rect = self.image.get_rect()
         self.rect.center = pos
         self.dx = dx
-        self.holds = package_items[random.randint(0, len(package_items) - 1)]
 
     def update(self):
         # move the package towards the left of the screen
@@ -17,3 +15,21 @@ class Package(pygame.sprite.Sprite):
 
     def display(self):
         main_s.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class EnemyDrop(Package):
+    def __init__(self, dx, pos):
+        super(EnemyDrop, self).__init__(dx, pos)
+        # pick a random item excluding the last option in the list which is health
+        self.holds = package_items[random.randint(0, len(package_items) - 2)]
+        self.image.fill((0, 0, 150))
+
+
+class HealthPack(Package):
+    def __init__(self, dx, pos):
+        super(HealthPack, self).__init__(dx, pos)
+        # hold the item health
+        self.holds = package_items[len(package_items) - 1]
+        self.image.fill((0, 150, 0))
+
+
