1a10f86f159b9464a1f645ee3b58538b315561dc
Joe <YJoe762@gmail.com>
2015-10-21 11:10:43 +0100

    Update Start_up.py

--- Desktop/Python/space_scroll/Start_up.py
+++ Desktop/Python/space_scroll/Start_up.py
@@ -1,53 +1,46 @@
-import pygame
-import random
-import math
-from Settings import Settings
-pygame.init()
-clock = pygame.time.Clock()
+from Start_up import*
 
+def create_stars(setting):
+    star_list = []
+    # star chance * 7 fills the screen with roughly the same amount of stars
+    # as starts per update would give, star amount can be changed in startup.py
+    for x in range(0, initial_stars):
+        if setting == "game":
+            # create stars moving from right to left
+            s = Star(random.randint(0, width),   # x pos
+                     random.randint(0, height),  # y pos
+                     random.randint(1, 2),       # dx
+                     0)                          # dy
+        elif setting == "menu":
+            # create slower stars moving diagonaly
+            r = random.choice([0.5, 1])
+            s = Star(random.randint(0, width),   # x pos
+                     random.randint(0, height),  # y pos
+                     r,                          # dx
+                     r)                          # d
+        star_list.append(s)
+    return star_list
 
-def deg_to_rad(degrees):
-    return degrees / 180.0 * math.pi
+class Star(pygame.sprite.Sprite):
+    def __init__(self, x, y, dx, dy):
+        random_size = random.randint(1, 2)
+        self.surface = pygame.Surface((random_size, random_size))
+        self.surface.fill((150, 150, 150))
+        self.rect = self.surface.get_rect()
+        self.exact_x = x
+        self.exact_y = y
+        self.rect.x = x
+        self.rect.y = y
+        self.dx = -dx
+        self.dy = -dy
 
-# variables to control game states
-home_state = 1
-info_state = 2
-game_state = 3
-pause_state = 4
-shop_state = 5
-settings_state = 6
-quit_state = 7
-game_over_state = 8
-reset_game_state = 9
+    def update(self):
+        # use of the exact position because rects can not hold float, if a
+        # speed were a decimal it would not be added to the position correctly
+        self.exact_x += self.dx
+        self.exact_y += self.dy
+        self.rect.x = self.exact_x
+        self.rect.y = self.exact_y
 
-# colours used
-main_theme = (0, 100, 100)
-red = (100, 0, 0)
-white = (255, 255, 255)
-
-# game settings
-settings = Settings()
-show_notes = True
-star_chance = 10  # chance of a star is 1 in 10 every update
-initial_stars = 100  # create 100 stars to fill the screen
-package_chance = 10  # chance of a package drop is 1 in 10 every enemy kill
-# if your game runs slow, set particle count low and star chance higher
-# all particles, stars, enemies and bullets are removed when they leave the screen
-
-width = 800
-height = 500
-main_s = pygame.display.set_mode((width, height))
-main_s.fill((255, 255, 255))
-pygame.display.set_caption("SPACE SHIPS")
-pygame.display.set_icon(pygame.image.load("Images/Icon.png"))
-font = pygame.font.Font("./Font/tall bolder.ttf", 15)
-menu_font = pygame.font.Font("./Font/tall bolder.ttf", 25)
-title_font = pygame.font.Font("./Font/tall bolder.ttf", 45)
-
-screen_rect = pygame.sprite.Sprite()
-# screen_rect is slightly bigger than the screen so that objects do not get removed
-# when created off screen. many objects are checked as still on screen to remove it
-# from a list that it is in
-screen_rect.rect = pygame.Surface((width + 30, height + 30)).get_rect()
-screen_rect.rect.x = 0
-screen_rect.rect.y = 0
+    def display(self):
+        main_s.blit(self.surface, (self.rect.x, self.rect.y))
